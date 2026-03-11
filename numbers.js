0ac015dc0614c9f874f72100df5f253bf535ffba
vilort1337-droid <vilort1337@gmail.com>
2026-03-11 08:05:33 +0000

    pupupu

---
+++ numbers.js
@@ -0,0 +1,10 @@
+var temperature = 36.6789 
+console.log (temperature.toFixed(1))
+var a = 17
+var b = 5 
+console.log (Math.floor(a/b))
+console.log(a%b)
+console.log("сто"-5)
+console.log(isNaN("сто" - 5 ))
+console.log(Math.max(14, -3, 77, 0, -100, 42))
+console.log(Math.min(14, -3, 77, 0, -100, 42))
---
+++ strings.js
@@ -0,0 +1,6 @@
+const sentense = " javascript — это просто! "
+console.log(sentense.trim().toUpperCase()[0] + sentense.trim().slice(1))
+const lit1 = "Иванов Иван" 
+const lit2 = "ИСР-21"
+const lit3 = 4.75
+console.log(`Студент ${lit1} группа ${lit2} средний балл: ${lit3}`)
